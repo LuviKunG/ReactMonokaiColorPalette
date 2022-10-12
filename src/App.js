4eae39fb37b4c64bd2b03e772c163ed7ec5721b3
Thanut Panichyotai <luvikung@gmail.com>
2022-10-12 12:57:43 +0700

    add new code color of monokai vscode. add new copy to clipboard both of hex and rgb.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,48 @@
 import React from 'react';
 import './App.css';
 import ColorBlock from './ColorBlock';
+import DisplayColor from './DisplayColor';
 import colorDefault from './colors/default';
 import colorBright from './colors/bright';
+import colorVSCode from './colors/vscode';
 import colorNeo from './colors/neo';
 
 function App() {
-  const [showMessage, setShowMessage] = React.useState(false);
+  const [showFlashMessage, setShowFlashMessage] = React.useState(false);
+  const [message, setMessage] = React.useState(null);
   const [color, setColor] = React.useState(null);
   React.useEffect(() => {
-    if (showMessage) {
-      setTimeout(() => {
-        setShowMessage(false);
-      }, 1000);
-    }
-  }, [showMessage]);
+    const timer = setTimeout(() => {
+      setShowFlashMessage(false);
+    }, 1000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [showFlashMessage]);
   const colors = {
     "Default": colorDefault,
     "Bright": colorBright,
     "Neo": colorNeo,
+    "VSCode": colorVSCode
   };
   const onColorSelected = (e, color) => {
-    console.log('copied to clipboard: ' + color);
-    navigator.clipboard.writeText(color);
-    setShowMessage(true);
     setColor(color);
   };
-  const hexToRgb = (hex) => {
-    const r = parseInt(hex.substr(1, 2), 16);
-    const g = parseInt(hex.substr(3, 2), 16);
-    const b = parseInt(hex.substr(5, 2), 16);
-    return `rgb(${r}, ${g}, ${b})`;
+  const onColorHexClicked = (e, color) => {
+    navigator.clipboard.writeText(color);
+    setMessage('HEX copied to clipboard');
+    setShowFlashMessage(true);
+  };
+  const onColorRGBClicked = (e, color) => {
+    const hexToRgb = (hex) => {
+      const r = parseInt(hex.substr(1, 2), 16);
+      const g = parseInt(hex.substr(3, 2), 16);
+      const b = parseInt(hex.substr(5, 2), 16);
+      return `rgb(${r}, ${g}, ${b})`;
+    };
+    navigator.clipboard.writeText(hexToRgb(color));
+    setMessage('RGB copied to clipboard');
+    setShowFlashMessage(true);
   };
   return (
     <div className="App">
@@ -57,10 +69,20 @@ function App() {
           }
         </div>
         {
-          color && <div className="DisplayColor"><p>{color} | {hexToRgb(color)}</p></div>
+          color &&
+          <div className='FixedFlex' style={{ bottom: '120px' }}>
+            <div className='FixedFlex-item'>
+              <DisplayColor color={color} onHEXClick={onColorHexClicked} onRGBClick={onColorRGBClicked} />
+            </div>
+          </div>
         }
         {
-          showMessage && <div className="FlashMessage"><p>Color copied to clipboard</p></div>
+          showFlashMessage &&
+          <div className='FixedFlex' style={{ bottom: '80px' }}>
+            <div className='FixedFlex-item'>
+              {message}
+            </div>
+          </div>
         }
       </body>
     </div>
---
+++ src/DisplayColor.js
@@ -0,0 +1,28 @@
+import './DisplayColor.css';
+
+function DisplayColor({ color, onHEXClick, onRGBClick }) {
+  const hexToRgb = (hex) => {
+    const r = parseInt(hex.substr(1, 2), 16);
+    const g = parseInt(hex.substr(3, 2), 16);
+    const b = parseInt(hex.substr(5, 2), 16);
+    return `rgb(${r}, ${g}, ${b})`;
+  };
+  const onHEXClicked = (e) => {
+    onHEXClick?.(e, color);
+  }
+  const onRGBClicked = (e) => {
+    onRGBClick?.(e, color);
+  }
+  return (
+    <div className="DisplayColor">
+      <button className="DisplayColor-button" onClick={onHEXClicked}>
+        <span className="DisplayColor-button-text">{color}</span>
+      </button>
+      <button className="DisplayColor-button" onClick={onRGBClicked}>
+        <span className="DisplayColor-button-text">{hexToRgb(color)}</span>
+      </button>
+    </div>
+  );
+}
+
+export default DisplayColor;
